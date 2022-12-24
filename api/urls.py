a45a01d17627e1df5bc18eab7c8196e8a3ab424c
Adnan Hussain <adnanrajput42@gmail.com>
2022-12-25 00:30:38 +0500

    get api

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,8 @@
-# from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    # path('', getRoutes, name='routes')
+    path('notes/', views.getNotes, name='notes'),
+    path('notes/<str:pk>/', views.getNote, name='note'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api.serializers import NoteSerializer
+from .models import Note
 
 # Create your views here.
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
